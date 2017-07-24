273924dec934f1493564b27050d6848c73f7e610
vagrant <vagrant>
2017-07-24 14:06:02 +0530

    AddSourceCode

---
+++ JenkinsTrial/JenkinsTrial/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace JenkinsTrial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text = ConfigurationManager.AppSettings[cmbSelectConfigKey.Text];
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            MathematicsOperation maths = new MathematicsOperation();
+            int result = maths.Add(int.Parse(txtA.Text), int.Parse(txtB.Text));
+            txtStatus.Text = result.ToString();
+        }
+    }
+}
