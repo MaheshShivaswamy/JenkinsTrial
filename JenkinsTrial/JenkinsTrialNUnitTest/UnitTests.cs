7a3630a89c49f153196a1c0e99936920fb5bae30
MaheshShivaswamy <maheshivaswamy@yahoo.co.in>
2017-09-02 15:03:00 +0530

    NUnit test checkin

---
+++ JenkinsTrial/JenkinsTrialNUnitTest/UnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JenkinsTrial;
+
+namespace JenkinsTrialNUnitTest
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            int A = 2;
+            int B = 3;
+            int expected = 5;
+
+            MathematicsOperation maths = new MathematicsOperation();
+            int actual = maths.Add(A, B);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
